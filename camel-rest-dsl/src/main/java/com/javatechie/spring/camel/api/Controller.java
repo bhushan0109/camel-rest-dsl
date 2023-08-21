862be858924fa3fd35eb87e447ede5fe1012a25f
bhushan0109 <bhupatil0001@gmail.com>
2023-08-21 21:14:38 +0530

    code updated

--- camel-rest-dsl/src/main/java/com/javatechie/spring/camel/api/Controller.java
+++ camel-rest-dsl/src/main/java/com/javatechie/spring/camel/api/Controller.java
@@ -30,7 +30,7 @@ public class Controller {
 	@Autowired
 	private CamelContext camelContext;
 
-	@GetMapping("/umnHash/{id}")
+	@GetMapping("/umnHash/")
 	public DingDto umnHash(@PathVariable int id) throws JsonParseException, JsonMappingException, IOException {
 		DingDto dingDto = new DingDto();
 		Exchange reqExchange = ExchangeBuilder.anExchange(camelContext).build();
@@ -46,38 +46,38 @@ public class Controller {
 	}
 
 	// not used
-	@GetMapping(value = "/my/{id}")
-	@ResponseStatus(value = HttpStatus.OK)
-	public DingDto myString(@PathVariable int id) {
-		DingDto dingDto = new DingDto();
-		if (id == 10) {
-			dingDto.setUmnHash("jghjksfdgsdgfkjasdgfvjadgjcvhjchadjcbhahcjad");
-		} else {
-			dingDto.setUmnHash("id not correct");
-		}
-		return dingDto;
-	}
-	@GetMapping("/number/{id}")
-	public DingDto number(@PathVariable int id) throws JsonParseException, JsonMappingException, IOException {
-		DingDto dingDto = new DingDto();
-		Exchange exchange = producerTemplate.send("http://localhost:9090/my/" + id, new Processor() {
-			public void process(Exchange exchange) throws Exception {
-			}
-		});
-
-		Message out = exchange.getOut();
-		int responseCode = out.getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class);
-		System.out.println("Response: " + String.valueOf(responseCode));
-		if (responseCode == 200) {
-			String body = exchange.getMessage().getBody(String.class);
-			dingDto = new ObjectMapper().readValue(body.toString(), DingDto.class);
-			String umnHash = dingDto.getUmnHash();
-			System.out.println("final umnHash output ==>" + umnHash);
-			System.out.println("DONE!!");
-		} else {
-			System.out.println("fail to get!!");
-		}
-		return dingDto;
-	}
+//	@GetMapping(value = "/my/{id}")
+//	@ResponseStatus(value = HttpStatus.OK)
+//	public DingDto myString(@PathVariable int id) {
+//		DingDto dingDto = new DingDto();
+//		if (id == 10) {
+//			dingDto.setUmnHash("jghjksfdgsdgfkjasdgfvjadgjcvhjchadjcbhahcjad");
+//		} else {
+//			dingDto.setUmnHash("id not correct");
+//		}
+//		return dingDto;
+//	}
+//	@GetMapping("/number/{id}")
+//	public DingDto number(@PathVariable int id) throws JsonParseException, JsonMappingException, IOException {
+//		DingDto dingDto = new DingDto();
+//		Exchange exchange = producerTemplate.send("http://localhost:9090/my/" + id, new Processor() {
+//			public void process(Exchange exchange) throws Exception {
+//			}
+//		});
+//
+//		Message out = exchange.getOut();
+//		int responseCode = out.getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class);
+//		System.out.println("Response: " + String.valueOf(responseCode));
+//		if (responseCode == 200) {
+//			String body = exchange.getMessage().getBody(String.class);
+//			dingDto = new ObjectMapper().readValue(body.toString(), DingDto.class);
+//			String umnHash = dingDto.getUmnHash();
+//			System.out.println("final umnHash output ==>" + umnHash);
+//			System.out.println("DONE!!");
+//		} else {
+//			System.out.println("fail to get!!");
+//		}
+//		return dingDto;
+//	}
 
 }
